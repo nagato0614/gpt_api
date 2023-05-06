c2932b8f2d7c25c82cb90bd4281dd39e3a5db284
nagato0614 <nagato0614@gmail.com>
2023-05-06 11:46:09 +0900

    [update]add get test

--- youtube_summary/test.py
+++ youtube_summary/test.py
@@ -3,6 +3,12 @@ YoutubeSummaryのテストモジュール
 """
 from django.test import TestCase
 from .models import YoutubeSummary
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from .views import YoutubeSummaryViewSet
 
 
 class YoutubeSummaryTestCase(TestCase):
@@ -23,9 +29,26 @@ class YoutubeSummaryTestCase(TestCase):
         )
         obj.save()
 
+        self.factory = APIRequestFactory()
+        self.view = YoutubeSummaryViewSet.as_view({'get': 'list'})
+        self.item_url = "youtube_summary/"
+        self.host_url = "https://127.0.0.1:8000/"
+
     def test_youtube_summary(self):
         """
         YoutubeSummaryのテスト
         """
         qs_count = YoutubeSummary.objects.count()
+        print("qs_count : ", qs_count)
         self.assertEqual(qs_count, 1)
+
+    def test_api_get(self):
+        """
+        APIのGETメソッドのテスト
+        :return:
+        """
+        url = self.host_url + self.item_url
+        request = self.factory.get(url)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
