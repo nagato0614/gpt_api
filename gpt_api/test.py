4ab5bda76d48aceaca7665fca8be6993c1254a6f
nagato0614 <nagato0614@gmail.com>
2023-05-06 19:52:35 +0900

    [update]add lib for requierment.txt

--- gpt_api/test.py
+++ gpt_api/test.py
@@ -10,6 +10,7 @@ from rest_framework.test import APIClient
 from rest_framework.test import APIRequestFactory
 from .views import *
 
+
 class YoutubeSummaryTestCase(TestCase):
     """
     YoutubeSummaryのテストクラス
